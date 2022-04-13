4354a8808743824c6830a11360b518ddb897b95b
[LL] Benjamin CORDREAUX <cordreaux.benjamin@gmail.com>
2022-04-14 01:52:04 +0200

    Nodemailer configured with template hbs for sendend mail with contact form

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const mainRouter = require("express").Router();
 const newsRouter = require("./news.routes");
 const articlesRouter = require("./articles.routes");
 const eventsRouter = require("./events.routes");
+const emailRouter = require("./emails.routes");
 
 mainRouter.use("/news", newsRouter);
 mainRouter.use("/articles", articlesRouter);
 mainRouter.use("/events", eventsRouter);
+mainRouter.use("/emails", emailRouter);
 
 module.exports = mainRouter;
