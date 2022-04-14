46dec52cf04d7e36ea292d4de4fcafd21a82d148
[LL] Benjamin CORDREAUX <cordreaux.benjamin@gmail.com>
2022-04-14 23:02:54 +0200

    Code linter et formater

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.listen(process.env.PORT || 8000, (err) => {
   console.log(`La connexion au serveur a réussi sur le port http://localhost:${process.env.PORT || 8000}`);
 
   // Test connexion to MYSQL DB
-  connection.connect((err) => {
-    if (err) return console.log(err.message);
-    console.log(`La connexion MySQL à la base de données ${process.env.DB_NAME} de ${process.env.DB_USER} a réussi`);
+  return connection.connect((err2) => {
+    if (err2) return console.log(err2.message);
+    return console.log(`La connexion MySQL à la base de données ${process.env.DB_NAME} de ${process.env.DB_USER} a réussi`);
   });
 });
 
